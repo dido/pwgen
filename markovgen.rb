ebe7051751151b26e39bd1e03ae1b635c69a7539
stormwyrm <stormwyrm@gmail.com>
2024-08-30 22:36:16 +0800

    calculate probabilities

--- markovgen.rb
+++ markovgen.rb
@@ -5,60 +5,39 @@
 # that can be used to generate other words that should conform
 # closely to the same phonology more or less.
 #
-# The letters are converted to lowercase and then numbered from 0 to
-# 25.
-# The Markov chain 
-def inc_tblidx(mtx, indices)
-  m = mtx
-  while indices.length > 1
-    m = m[indices.shift]
-  end
-  idx = indices.shift
-  m[idx] += 1.0
-end
 
-def tblidx(mtx, indices)
-  m = mtx
-  indices.each do |idx|
-    m = m[idx]
-  end
-  return(m)
+def add_transition(matrix, state, nstate)
+#  p [state, nstate]
+  matrix[state] ||= Hash.new
+  matrix[state][nstate] ||= 0
+  matrix[state][nstate] += 1
 end
 
 MAXCHARS = 26
-context = ARGV[0].to_i
-# generate an empty multidimensional array with 0.0 for all values
-def gen_array(depth)
-  return(Array.new(MAXCHARS, 0.0)) if (depth <= 0)
-  a = Array.new(MAXCHARS)
-  0.upto(MAXCHARS-1) {|i| a[i] = gen_array(depth-1) }
-  return(a)
-end
-matrix = gen_array(context-1)
-count = 0
+clen = ARGV[0].to_i
+matrix = {}
 STDIN.each do |line|
   line.chomp!
   line.downcase!
   next unless line =~ /^[a-z]+$/
-  letters = line.unpack("c*").map {|x| x - 97 }
-  letters.each_index do |i|
-    ctx = []
-    0.upto(context-1) do |j|
-      break if i+j >= letters.length
-      ctx << letters[i+j]
-    end
-    next if ctx.length < context
-    #p (ctx.map{|x| x+ 97}).pack("c*")
-    inc_tblidx(matrix, ctx)
-    count += 1
+  state = :start
+  nstate = ""
+  ch = 0
+#  p line
+  loop do
+    nstate = line[ch,clen]
+    break if nstate.nil? || nstate.empty?
+    add_transition(matrix, state, nstate)
+    state = nstate.clone
+    ch += clen
+  end
+  add_transition(matrix, state, :end)
+end
+# Normalize the state counts
+matrix.each_key do |k|
+  sum = matrix[k].each_value.sum
+  matrix[k].each_key do |kk|
+    matrix[k][kk] /= sum.to_f
   end
 end
-# Normalize the matrix
-def normalize(matrix, div)
-  if matrix[0].is_a?(Array)
-    return(matrix.map {|x| normalize(x, div) })
-  end
-  return(matrix.map {|x| x/div})
-end
-matrix = normalize(matrix, count)
 p matrix
